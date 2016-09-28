b75403281f067a8b06bf05b8999ec697abe086aa
aspiringguru <bmatthewtaylor@gmail.com>
2016-09-28 18:31:01 +1000

    Deploying Stemming

--- tools/parse_out_email_text_original.py
+++ tools/parse_out_email_text_original.py
@@ -28,28 +28,12 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        #words = text_string
-        from nltk.stem.snowball import SnowballStemmer
-        print("available languages =", " ".join(SnowballStemmer.languages))
-        #stemmer = SnowballStemmer("english")
-        stemmer = SnowballStemmer("english", ignore_stopwords=True)
-        print "type(stemmer)=", type(stemmer)
-        print "type(content)=", type(content)
-        print "type(text_string)=", type(text_string), "text_string=", text_string
-        text_stringWords = text_string.split(" ")
-        print "type(text_stringWords)=", type(text_stringWords), "len(text_stringWords)=", len(text_stringWords)
-        for myword in text_stringWords:
-            myword = myword.rstrip()
-            myword = myword.strip()
-            print "myword='{0}' , stemmer.stem(myword)='{1}'".format(myword, stemmer.stem(myword))
-            if len(myword)>0:
-                words += " "+stemmer.stem(myword)
-        words = words.strip()
+        words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        print "words=", words
+        
 
 
 
