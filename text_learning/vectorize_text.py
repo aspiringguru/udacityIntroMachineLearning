28d77c87c748c21661008ae4763f0316e7795c82
aspiringguru <bmatthewtaylor@gmail.com>
2016-09-29 13:35:11 +1000

    lesson 10 text learning final - still not perfect

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -69,8 +69,9 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            wordsToRemove = ["sara", "shackleton", "chris", "germani"]
             stemmedText = stemmedText.lower()
-            for word in ["sara", "shackleton", "chris", "germani"]:
+            for word in wordsToRemove:
                 stemmedText = stemmedText.replace(word, "")
             #print "stemmedText after stripping words = ", stemmedText
 
@@ -111,25 +112,55 @@ from nltk.corpus import stopwords
 sw = stopwords.words("english")
 
 print "type(sw)=", type(sw)
+print "type(sw[0])=", type(sw[0])
 print "len(sw)=", len(sw)
+print "sw=", sw
+sw = sw + wordsToRemove
+print "after adding 'wordsToRemove' len(sw)=", len(sw)
 
 #Remove english stopwords.
 
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-#vectorizer = TfidfVectorizer(stop_words="english",lowercase=True)
-sw = stopwords.words("english")
-vectorizer = TfidfVectorizer(stop_words = sw)
+vectorizer = TfidfVectorizer(stop_words = sw, lowercase=True)
+#vectorizer = TfidfVectorizer(stop_words="english", lowercase=True)
+
+#NB lowercase : boolean, default True
+
 vectorizer.fit_transform(word_data)
 # bag_words = vectorizer.transform(word_data)
 
 # Get how many unique words there are in the emails
-print len(vectorizer.get_feature_names())
+print "type(vectorizer.get_feature_names())=", type(vectorizer.get_feature_names())
+#list
+#for i in range(10):
+#    print "type(vectorizer.get_feature_names()[{}])=".format(i), type(vectorizer.get_feature_names()[i])
+#    print "vectorizer.get_feature_names()[{}])=".format(i), vectorizer.get_feature_names()[i]
+#NB this showed the elements of vectorizer.get_feature_names() are not strings.
 
-# Word number 34597
-print "word[34597]=", vectorizer.get_feature_names()[34597]
-#
+print "len(vectorizer.get_feature_names())=", len(vectorizer.get_feature_names())
+#incorrect answers so far.
 #41976
 #42117
 #44993
-#
+#41973
+
+#How many different words are there?
+print "size of set = ", len(set(vectorizer.get_feature_names()))
+#size of set =  42117
+
+# Word number 34597
+print "word[34597]=", vectorizer.get_feature_names()[34597]
+
+#check if element matches elements of wordsToRemove = ["sara", "shackleton", "chris", "germani"]
+
+
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer, TfidfVectorizer
+
+vectorizer = TfidfVectorizer(stop_words="english")
+X = vectorizer.fit_transform(word_data)
+transformer = TfidfTransformer()
+tfidf = transformer.fit_transform(X)
+print "BBBBB", len(vectorizer.get_feature_names())
+
+
