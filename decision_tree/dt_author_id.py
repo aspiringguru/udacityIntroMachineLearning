1ef787819ad6ab66ef776e05dc1ddd7b8e554694
aspiringguru <bmatthewtaylor@gmail.com>
2016-10-05 15:59:39 +1000

    validation exercises added

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -17,14 +17,33 @@ from email_preprocess import preprocess
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
+
 features_train, features_test, labels_train, labels_test = preprocess()
 
+#find the number of features in your data.
+print ("type(features_train)=", type(features_train))
+print ("type(features_test)=", type(features_test))
+print ("type(labels_train)=", type(labels_train))
+print ("type(labels_test)=", type(labels_test))
 
-
+print ("len(features_train[0])=", len(features_train[0]))
 
 #########################################################
 ### your code goes here ###
+#Get the decision tree up and running as a classifier,
+# setting min_samples_split=40.
+# It will probably take a while to train.
+# Whats the accuracy?
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+start_time = time()
+clf = clf.fit(features_train, labels_train)
+print("--- time to clf.fit %s seconds ---" % (time() - start_time))
 
+start_time = time()
+acc =  clf.score(features_test, labels_test)
+print("--- time to clf.score %s seconds ---" % (time() - start_time))
+print ("acc from tree.DecisionTreeClassifier = ", acc)
 
 #########################################################
 
--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -9,6 +9,7 @@
 
     After that, it's not our code anymore--it's yours!
 """
+from time import time
 
 import pickle
 import sys
@@ -22,16 +23,44 @@ data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") 
 ### have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
 
-data = featureFormat(data_dict, features_list)
+data = featureFormat(data_dict, features_list)#original
+#data = featureFormat(data_dict, features_list, sort_keys = '../tools/python2_lesson13_keys.pkl')
+#
+
 labels, features = targetFeatureSplit(data)
 
 
 
 ### it's all yours from here forward!  
 from sklearn import tree
+
+start_time = time()
 clf = tree.DecisionTreeClassifier()
+print("--- time to initialise tree.DecisionTreeClassifier %s seconds ---" % (time() - start_time))
+
+start_time = time()
 clf = clf.fit(features, labels)
+print("--- time to clf.fit %s seconds ---" % (time() - start_time))
 
+start_time = time()
 acc =  clf.score(features, labels)
+print("--- time to clf.score %s seconds ---" % (time() - start_time))
 
+print ("acc=", acc)
+
+#validation 18 of 18 Deploying a Training/Testing Regime
+#hold out 30% of the data for testing and set the random_state parameter to 42
+#from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
+
+
+start_time = time()
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+print("--- time to train_test_split %s seconds ---" % (time() - start_time))
+print "type(X_train)=", type(X_train), len(X_train)
+print "type(X_test)=", type(X_test), len(X_test)
+print "type(y_train)=", type(y_train), len(y_train)
+print "type(y_test)=", type(y_test), len(y_test)
+
+acc =  clf.score(X_test, y_test)
 print ("acc=", acc)
